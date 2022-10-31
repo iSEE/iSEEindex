1670c1703c15b746f026beb38f9ad7b042f67860
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2022-10-31 10:15:11 +0000

    add function to load data set

--- R/utils-datasets.R
+++ R/utils-datasets.R
@@ -1,13 +1,13 @@
 #' Available Data Sets
 #'
-#' Import metadata for available data sets.
+#' Import and process metadata for available data sets.
 #'
-#' @param FUN A function .
+#' @param FUN A function.
 #'
 #' @return
 #' `.datasets_available()` returns a `data.frame` of metadata for available data sets.
 #' 
-#' @examples 
+#' @examples
 #' ## Setup ----
 #' 
 #' dataset_fun <- function() {
@@ -21,4 +21,60 @@
 #' @rdname INTERNAL_datasets_available
 .datasets_available <- function(FUN) {
     FUN()
+}
+
+#' Load Object and Coerce to SingleCellExperiment
+#'
+#' @param bfc A 
+#' @param uri A URI as a character scalar.
+#'
+#' @return
+#' `.load_sce()` returns a [SingleCellExperiment()] object.
+#'
+#' @import SingleCellExperiment
+#' @importFrom BiocFileCache bfcadd bfcquery
+#'
+#' @rdname INTERNAL_load_sce
+#' @examples
+#' ## Setup ----
+#' 
+#' library(BiocFileCache)
+#' bfc <- BiocFileCache()
+#' uri <- "https://zenodo.org/record/7186593/files/ReprocessedAllenData.rds?download=1"
+#' 
+#' ## Usage ---
+#' 
+#' iSEEindex:::.load_sce(bfc, uri)
+#' 
+.load_sce <- function(bfc, uri) {
+    bfc_result <- bfcquery(bfc, uri, field = "rname", exact = TRUE)
+    # nocov start
+    if (nrow(bfc_result) == 0) {
+        object_path <- bfcadd(bfc, uri)
+    } else {
+        object_path <- bfc[[bfc_result$rid]]
+    }
+    # nocov end
+    object <- readRDS(object_path)
+    object <- .convert_to_sce(object)
+    object
+}
+
+#' @param x An object Coercible to SingleCellExperiment
+#'
+#' @return
+#' For `.convert_to_sce()`, a [SingleCellExperiment()] object.
+#'
+#' @importFrom methods is as
+#' @importFrom SummarizedExperiment SummarizedExperiment
+#'
+#' @rdname INTERNAL_load_sce
+.convert_to_sce <- function(x) {
+    if (!is(x, "SummarizedExperiment")) {
+        x <- as(x, "SummarizedExperiment")
+    }
+    if (!is(x, "SingleCellExperiment")) {
+        x <- as(x, "SingleCellExperiment")
+    }
+    x
 }
---
+++ tests/testthat/test-utils-datasets.R
@@ -0,0 +1,45 @@
+# .datasets_available ----
+
+test_that(".datasets_available works", {
+    
+    dataset_fun <- function() {
+        read.csv(system.file(package = "iSEEindex", "datasets.csv"))
+    }
+    
+    out <- iSEEindex:::.datasets_available(dataset_fun)
+    
+    expect_s3_class(out, "data.frame")
+    expect_identical(nrow(out), 1L)
+})
+
+# .load_sce ----
+
+test_that(".load_sce works", {
+    
+    ## Setup ----
+
+    bfc <- BiocFileCache::BiocFileCache()
+    url <- "https://zenodo.org/record/7186593/files/ReprocessedAllenData.rds?download=1"
+    
+    ## Usage ---
+    
+    out <- iSEEindex:::.load_sce(bfc, url)
+    
+    expect_s4_class(out, "SummarizedExperiment")
+})
+
+# .convert_to_sce ----
+
+test_that(".convert_to_sce works for ExpressionSet", {
+
+    out <- iSEEindex:::.convert_to_sce(ExpressionSet())
+    expect_s4_class(out, "SingleCellExperiment")
+
+})
+
+test_that(".convert_to_sce works for SummarizedExperiment", {
+
+    out <- iSEEindex:::.convert_to_sce(SummarizedExperiment())
+    expect_s4_class(out, "SingleCellExperiment")
+
+})
