edcf6d3698e3e7498a3ced0b02c7e53332d18c69
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2023-04-20 16:38:10 +0100

    resolve #23; fix man page example; support non-overlapping yaml option names

--- R/utils-yaml.R
+++ R/utils-yaml.R
@@ -1,4 +1,14 @@
 #' Import Resource Metadata from YAML
+#' 
+#' `.list_to_dataframe()` converts a list of options for a set of configurations
+#' into a `data.frame` where each row represents one configuration.
+#' See Details.
+#' 
+#' @details
+#' `.list_to_dataframe()` supports sets of configurations with non-overlapping
+#' option names.
+#' The `colnames` of the output `data.frame` will be the union of all the
+#' options found across all the configurations in the input list.
 #'
 #' @param x A `list` of resource metadata.
 #'
@@ -22,7 +32,18 @@
 .list_to_dataframe <- function(x) {
     # Convert list to a list of data.frames
     list_of_df <- lapply(x, as.data.frame)
+    # Identify union of column names
+    list_of_colnames <- lapply(list_of_df, colnames)
+    unique_colnames <- unique(do.call("c", list_of_colnames))
+    # Fill missing columns with NA
+    list_of_df <- lapply(list_of_df, function(x) {
+        x[, setdiff(unique_colnames, colnames(x))] <- NA
+        x
+    })
     # rbind into a single data.frame
+    list_of_df <- lapply(list_of_df, function(x) 
+        x[, intersect(colnames(x), unique_colnames), drop = FALSE]
+    )
     df <- do.call("rbind", list_of_df)
     df
 }
